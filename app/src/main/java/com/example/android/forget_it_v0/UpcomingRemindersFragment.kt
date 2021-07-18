0db6b39e56acb1ccaa44c9f2c5d8b59dd2a12c9d
UVrajSB <mryuvrajsbhadauria@gmail.com>
2021-07-18 14:35:36 +0530

    final code

--- app/src/main/java/com/example/android/forget_it_v0/UpcomingRemindersFragment.kt
+++ app/src/main/java/com/example/android/forget_it_v0/UpcomingRemindersFragment.kt
@@ -22,6 +22,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.forget_it_v0.adapter.UpcomingAdapter
 import com.example.android.forget_it_v0.databinding.FragmentUpcomingRemindersBinding
+
 import com.example.android.forget_it_v0.models.*
 import com.example.android.forget_it_v0.repository.FirestoreRepo
 import com.google.firebase.auth.FirebaseAuth
